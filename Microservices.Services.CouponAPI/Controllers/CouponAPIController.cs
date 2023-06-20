f4594ca5e0a48454e1dacf73c5085e2fc3b7cc72
agkosd <sonawaneakshay039@gmail.com>
2023-06-20 23:20:56 +1000

    Add coupons api

---
+++ Microservices.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Microservices.Services.CouponAPI.Data;
+using Microservices.Services.CouponAPI.Models;
+using Microservices.Services.CouponAPI.Models.CouponDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Services.CouponAPI.Controllers
+{
+    [Route("api/v1/couponsAPI")]
+    [ApiController]
+    public class CouponAPIController:ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly ResponseDto _response;
+        private IMapper _mapper;
+
+        public CouponAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> coupons = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(coupons);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(_ => _.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code:string}")]
+        public ResponseDto Get(string code)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(_ => _.CouponCode == code);
+                if(coupon == null)
+                {
+                    _response.IsSuccess=false;
+                }
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+       
+    }
+}
---
+++ Microservices.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microservices.Services.CouponAPI.Models;
+using Microservices.Services.CouponAPI.Models.CouponDto;
+
+namespace Microservices.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
